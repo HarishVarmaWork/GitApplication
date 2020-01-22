bdfeff6f741aed7b5f2240fb3952bf3e282df79c
HarishVarmaWork <harishvarmawork@gmail.com>
2020-01-22 12:14:11 +0530

    LongClickCommitMessage

--- app/src/main/java/com/example/gitapplication/MainActivity.java
+++ app/src/main/java/com/example/gitapplication/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
         helloText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
