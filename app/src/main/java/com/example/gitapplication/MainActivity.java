9b0321fdc989a04449ac027b8f0e34a1532b1b8e
HarishVarmaWork <harishvarmawork@gmail.com>
2020-01-22 12:07:47 +0530

    Setting name

--- app/src/main/java/com/example/gitapplication/MainActivity.java
+++ app/src/main/java/com/example/gitapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView helloText = this.findViewById(R.id.helloText);
         helloText.setText("Hello");
+
+        helloText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 }
